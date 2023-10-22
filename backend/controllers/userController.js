dd36ee64af075f8d74703916a7b7f61cb83e5360
Shohail-Element <105637850+Shohail-Element@users.noreply.github.com>
2023-10-22 21:34:35 +0530

    email on registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,76 +3,107 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
 
+const sendMail = (email, subject) => {
+  var transporter = createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USERNAME,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  });
+
+  var mailOptions = {
+    from: "ahmedsu868@gmail.com",
+    to: email,
+    subject: subject,
+    html: `<h1>Registered successfully</h1></h3>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
 //create token
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: 3 * 24 * 60 * 60
-    })
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: 3 * 24 * 60 * 60,
+  });
+};
 
 //login user
-const loginUser = async (req,res) => {
-    const {email, password} = req.body;
-    try{
-        if(!email || !password){
-            return res.status(400).json({message: "Please enter all fields"})
-        }
-        const user = await userModel.findOne({email})
-
-        if(!user){
-            return res.status(400).json({message: "User does not exist"})
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password)
-        if(!isMatch){
-            return res.status(400).json({message: "Invalid credentials"})
-        }
-        const token = createToken(user._id)
-        res.status(200).json({user,token})
-    } catch (error) {
-        res.status(500).json({message: error.message})
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please enter all fields" });
     }
-}
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = createToken(user._id);
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 //register user
-const registerUser = async (req,res) => {
-    const {name, email, password} = req.body;
-    try{
-        //check if user already exists
-        const exists = await userModel.findOne({email})
-        if(exists){
-            return res.status(400).json({message: "User already exists"})
-        }
-        if (validator.isEmpty(name) || validator.isEmpty(email) || validator.isEmpty(password)) {
-            return res.status(400).json({message: "Please enter all fields"})
-        }
-        if(!validator.isEmail(email)){
-            return res.status(400).json({message: "Please enter a valid email"})
-        }
-        if(!validator.isStrongPassword(password)){
-            return res.status(400).json({message: "Please enter a strong password"})
-        }
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt)
-
-        const newUser = new userModel({name, email, password: hashedPassword})
-        const user = await newUser.save()
-        const token = createToken(user._id)
-        res.status(200).json({user,token})
-
-    } catch(error){
-        res.status(500).json({message: error.message})
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    //check if user already exists
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
+    if (
+      validator.isEmpty(name) ||
+      validator.isEmpty(email) ||
+      validator.isEmpty(password)
+    ) {
+      return res.status(400).json({ message: "Please enter all fields" });
+    }
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: "Please enter a valid email" });
+    }
+    if (!validator.isStrongPassword(password)) {
+      return res
+        .status(400)
+        .json({ message: "Please enter a strong password" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({ name, email, password: hashedPassword });
+    const user = await newUser.save();
+    sendMail(email, "Registered with todo list application");
+    const token = createToken(user._id);
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 //get user info
-const getUser = async (req,res) => {
-    const id = req.user.id
-    try{
-        const user = await userModel.find({_id:id})
-        res.status(200).json({user: user[0]})
-    } catch(error){
-        res.status(502).json({message: error.message})
-    }
-}
-export {loginUser, registerUser, getUser}
+const getUser = async (req, res) => {
+  const id = req.user.id;
+  try {
+    const user = await userModel.find({ _id: id });
+    res.status(200).json({ user: user[0] });
+  } catch (error) {
+    res.status(502).json({ message: error.message });
+  }
+};
+export { loginUser, registerUser, getUser };
